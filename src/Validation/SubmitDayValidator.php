470657a1c61e45eb57c7fb8e93a794fc3bdbe8f3
Istvan Drahos <drahos.istvan@gmail.com>
2017-10-23 23:24:51 +0200

    refactoring date validator

--- src/Validation/SubmitDayValidator.php
+++ src/Validation/SubmitDayValidator.php
@@ -9,22 +9,39 @@ use DueDateCalculator\Exceptions\Validation\InvalidWorkdayDateException;
 
 class SubmitDayValidator implements Validator
 {
+    private $date;
+
     public function validate($submitDay)
     {
+        $this->createDateFormat($submitDay)
+              ->validateWeekday()
+              ->validateWorkHours();
+
+        return $this->date;
+    }
+
+    private function createDateFormat($submitDay){
         try {
-            $validatedDay = Carbon::createFromFormat('Y-m-d H:i:s', $submitDay);
+            $this->date = Carbon::createFromFormat('Y-m-d H:i:s', $submitDay);
+            return $this;
         } catch (\Exception $e) {
             throw new InvalidDateFormatException('Submit date has not valid format');
         }
+    }
 
-        if ($validatedDay->isWeekend()) {
+    private function validateWeekday(){
+        if ($this->date->isWeekend()) {
             throw new InvalidWorkdayDateException('Submit day is in weekend');
         }
 
-        if (!($validatedDay->hour >= 9 && $validatedDay->hour < 17)) {
+        return $this;
+    }
+
+    private function validateWorkHours() {
+        if (!($this->date->hour >= 9 && $this->date->hour < 17)) {
             throw new InvalidWorkdayDateException('Submit day is out of working hours');
         }
 
-        return $validatedDay;
+        return $this;
     }
 }
