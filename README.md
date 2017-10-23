f446b4ab5f73f0697a8444c23654211fbfd55f8d
Istvan Drahos <drahos.istvan@gmail.com>
2017-10-23 20:07:15 +0200

    Added small console application for better "real life" testing

---
+++ README.md
@@ -0,0 +1,13 @@
+# Due Date Calculator
+
+## Install
+1. Clone the repository `git clone https://developer.isti.cloud/drahos.istvan/due-date-calculator.git`
+2. Install composer dependencies `composer install`
+3. Add proper rights to the application `chmod +x ./application`
+
+## Application run
+`./application duedate:calculate "2017-10-23 15:30:00" 2`
+
+__Argument 1:__ Submit day (format: YYYY-MM-DD HH:MM:SS)
+
+__Arugment 2:__ Turnaround time (in hours)
---
+++ src/DueDateCalculatorCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DueDateCalculator;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DueDateCalculatorCommand extends Command
+{
+    protected function configure(){
+        $this->setName("duedate:calculate")
+            ->setDescription("Calculate Due Date")
+            ->addArgument('Submit day', InputArgument::REQUIRED, 'Date in YYYY-MM-DD HH:MM:SS format')
+            ->addArgument('Turnaround time', InputArgument::REQUIRED, 'Number, represents the hour');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output){
+
+        $calculator = new DueDateCalculator();
+        $submitDate = $input->getArgument('Submit day');
+        $turnaroundTime = $input->getArgument('Turnaround time');
+
+        try {
+            $result = $calculator->calculate($submitDate, $turnaroundTime);
+        } catch (\Exception $e) {
+            $output->writeln("<error>{$e->getMessage()}</error>");
+            exit();
+        }
+
+        $output->writeln('Result: ' . $result);
+
+    }
+}
